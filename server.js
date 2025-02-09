cec83dbe79e3dc4e28e94bac7ffbfbeb5c3a4702
sourabh945 <sourabhsheokand945@gmail.com>
2025-02-09 17:56:48 +0530

    a

--- server.js
+++ server.js
@@ -2,7 +2,32 @@
 // author : sourabh945 <sheokand.sourabh.anil@gmail.com> <github.com/sourabh945>
 //
 require("dotenv").config();
+const express = require("express");
+const app = express();
 
-const userSequelize = require("./backend/config/db");
+const PORT = process.env.PORT || 3000;
 
-// console.log(userSequelize);
+// middleware
+
+app.use(express.json());
+
+// routes
+app.use("/api/v1/auth", require("./backend/routes/auth"));
+
+// databse connection
+const userDB = require("./backend/config/db");
+
+// error handler middleware
+app.use(require("./backend/middleware/errorHandler"));
+
+app.listen(PORT, async () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+  try {
+    await userDB.sync();
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.log("Database connection failed");
+    console.log(err);
+    process.exit(1);
+  }
+});
